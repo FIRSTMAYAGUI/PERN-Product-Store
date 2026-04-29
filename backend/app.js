5e0386876c2d3bb0df4c448c2c5414656d7607d3
FIRSTMAYAGUI <firstmayaguijunior@gmail.com>
2026-04-29 23:20:07 +0200

    refactor: moved package json and .gitignore files to backend folder

--- backend/app.js
+++ backend/app.js
@@ -12,18 +12,18 @@ you won't be able to do this "const { name, price, image } = req.body" */
 app.use(cors());
 console.log("Starting backend server...")
 
-const __dirname = path.resolve();
+//const __dirname = path.resolve();
 
 app.use("/api/product", productRoutes);
 
 //making the frontend and backend to run on the same port
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// if(process.env.NODE_ENV === "production"){
+//     app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-    app.get("/*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-    });
-}
+//     app.get("/*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+//     });
+// }
 
 const APP_PORT = process.env.APP_PORT || 5000;
 //npm install cross-env --save-dev
